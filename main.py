60ddb2179fbb09082419d0f06ac22e3a537050c6
Joe Gilbert <joegilbertdev@outlook.com>
2025-03-24 14:32:24 +0000

    Dataset is a conversational Language dataset

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from datasets import Dataset
-
 from LoraTrainingPipeline import LoraTrainingPipeline
 
 # hf_model = # "mistralai/Mistral-7B-Instruct-v0.3"
-runner = LoraTrainingPipeline(device='0', hf_model="pankajmathur/orca_mini_3b"   )
+runner = LoraTrainingPipeline(device="0", hf_model="pankajmathur/orca_mini_3b")
 runner.run()
